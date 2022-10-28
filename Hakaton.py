4ffe48dd1947744f73c79971dfbd75944f2ef160
Илья Старшинов <91202775+IlyaStarshinov@users.noreply.github.com>
2022-10-28 22:07:31 +0300

    Update Hakaton.py

--- Hakaton.py
+++ Hakaton.py
@@ -1,77 +1,85 @@
-from cgitb import handler
-from codecs import BOM_UTF16
-from unicodedata import name
-import aiogram
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher.filters import Text
-from aiogram.types import CallbackQuery, Message,\
-    InlineKeyboardButton, InlineKeyboardMarkup
-TOKEN = '5648532002:AAEfiIK-CuDJU76VdJ7ToKH0wzTcVhYCQd8'
+from email import message
+from turtle import setx
+from aiogram import types, executor, Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton   
 
+TOKEN = "5796328526:AAHwcp2XzN1oJ-G-05RwoughoBZ3PJAbJno"
+storage = MemoryStorage()
 bot = Bot(TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 
-HELP_COMMAND = """
-/help - список команд
-/start - начать работу с ботом
-/description - описание бота"""
-DESCRIPTION  = """Этот бот сделан Командой "Yellow Bogdan".
-Наше детище поможет тебе найти единомышленников в твоей сфере деятельности. Достаточно просто написать команду "/start"!"""
+def kb1():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton('HR','PR','Команда разработчкиков'))
+    return kb
 
-@dp.message_handler(commands=['help'])
-async def help_command(message: types.Message):
-    await message.answer(text=HELP_COMMAND)
-    await message.delete()
 
-@dp.message_handler(commands=['description'])
-async def description_command(message: types.Message):
-    await message.answer(text=DESCRIPTION)
-    await message.delete()
+def kb2():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton('Женский','Мужской'))
+    return kb
 
+
+def kb3():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton('IT','Путешествия','Спорт','Музыка'))
+    return kb
+
+class QuestionnaireStatesGroup(StatesGroup):
+    department = State()
+    sex = State()
+    age = State()
+    hobbies = State()
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    keyboard1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    bt1 = types.KeyboardButton(text = 'Выбор отдела')
-    keyboard1.add(bt1)
-    await message.answer(text=f'Привет, {message.chat.first_name} {message.chat.last_name}! Для начала нужно заполнить анкету', reply_markup = keyboard1)
-    await message.delete()
-
-@dp.message_handler(Text(equals="Выбор отдела"))
-async def areplynswer_bt1(message: types.Message):
-    keyboard2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    bt2 = types.KeyboardButton(text = 'HR')
-    bt3 = types.KeyboardButton(text = 'Анкета')
-    bt4 = types.KeyboardButton(text = 'Команда разработчиков')
-    keyboard2.add(bt2).add(bt3).add(bt4)
-    await message.answer(f"Я Вас понял, {message.chat.first_name}. Выберите, пожалуйста, ваш отдел", reply_markup = keyboard2)
-
-@dp.message_handler(Text(equals='Анкета'))
-async def questionnaire1(message: types.Message):
-    keyboard3 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    bt4 = types.KeyboardButton(text = 'MAN')
-    bt5 = types.KeyboardButton(text = 'GIRL')
-    keyboard3.add(bt4).add(bt5)
-    await message.answer('Какой у вас пол?', reply_markup=keyboard3)
-
-@dp.message_handler(Text(equals='MAN'or'GIRL'))
-async def questionnaire2(message: types.Message):
-    keyboard4 = types.ReplyKeyboardMarkup(resize_keyboard = True)
-    bt6 = types.KeyboardButton(text = 'Спорт')
-    bt7 = types.KeyboardButton(text = 'Шоппинг')
-    bt8 = types.KeyboardButton(text = 'Еда')
-    bt9 = types.KeyboardButton(text = 'IT')
-    keyboard4.add(bt6).add(bt7).add(bt8).add(bt9)
-    await message.answer('Чем вы любите увлекаться в свободное время?', reply_markup= keyboard4)
-
-    
+    await message.answer(f'Привет, {message.chat.first_name} {message.chat.last_name}! Для начала нужно заполнить анкету, для этого нажми /create')
 
 
+@dp.message_handler(commands=['create'])
+async def create_command(message: types.Message):
+    await message.answer('Давай же начнем! Для начала выбери свой отдел', reply_markup=kb1())
+    await QuestionnaireStatesGroup.department.set()
 
-    
+@dp.message_handler(content_types=['text'], state=QuestionnaireStatesGroup.department)
+async def load_department(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['department'] = message.text
+
+    await message.answer('Теперь укажи свой пол', reply_markup=kb2())
+    await QuestionnaireStatesGroup.next()
+
+@dp.message_handler(state=QuestionnaireStatesGroup.sex)
+async def load_sex(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['sex'] = message.text
+
+    await message.answer('Сколько тебе лет?')
+    await QuestionnaireStatesGroup.next
+
+@dp.message_handler(state=QuestionnaireStatesGroup.age)
+async def load_age(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['age'] = message.text
+
+    await message.answer('Теперь укажи своё хобби', reply_markup=kb3())
+    await QuestionnaireStatesGroup.next
+
+@dp.message_handler(state=QuestionnaireStatesGroup.hobbies)
+async def load_hobbies(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['hobbies'] = message.text
+        await bot.send_message(chat_id=message.from_user.id, caption = f"{data['department']},{data['sex']},{data['age']},{data['hobbies']}")
+
+        await message.answer('Ваша анкета успешно создана!)')
+        await state.finish()
 
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
  
+
