def0cd5f9b7965808f9b56aee66a4fb56a6fad0f
Илья Старшинов <91202775+IlyaStarshinov@users.noreply.github.com>
2022-10-29 13:35:04 +0300

    Update Hakaton.py

--- Hakaton.py
+++ Hakaton.py
@@ -1,4 +1,6 @@
 from email import message
+import re
+from tracemalloc import start
 from turtle import setx
 from aiogram import types, executor, Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -14,22 +16,35 @@ storage = MemoryStorage()
 bot = Bot(TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
-def kb1() -> ReplyKeyboardMarkup:
+
+dep = ['HR','PR','Команда разработчиков']
+sx = ['Мужской','Женский']
+hob = ['IT','Спорт','Путешествия']
+ans = ['Да','Нет']
+create = ['/create']
+
+def kb1(items: list[str]) -> ReplyKeyboardMarkup:    
+
+    
+    row = [KeyboardButton(text=item) for item in items]
+    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+
+
+def get_cancel_kb() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(KeyboardButton('HR','PR','Команда разработчкиков'))
+    kb.add(KeyboardButton('/cancel'))
+
     return kb
+#def kb2() -> ReplyKeyboardMarkup:
+#    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+#    kb.add(KeyboardButton('Женский','Мужской'))
+ #   return kb
 
 
-def kb2() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(KeyboardButton('Женский','Мужской'))
-    return kb
-
-
-def kb3()-> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(KeyboardButton('IT','Путешествия','Спорт','Музыка'))
-    return kb
+#def kb3()-> ReplyKeyboardMarkup:
+#    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+ #   kb.add(KeyboardButton('IT','Путешествия','Спорт','Музыка'))
+ #   return kb
 
 class QuestionnaireStatesGroup(StatesGroup):
     department = State()
@@ -37,14 +52,22 @@ class QuestionnaireStatesGroup(StatesGroup):
     age = State()
     hobbies = State()
 
+@dp.message_handler(commands=['cancel'], state='*')
+async def cancel_command(message: types.Message, state: FSMContext ):
+    if state is None:
+        return
+    await state.finish()
+    await message.answer('Вы прервали создание анкеты!', reply_markup=kb1(create))
+
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message) -> None:
-    await message.answer(f'Привет, {message.chat.first_name} {message.chat.last_name}! Для начала нужно заполнить анкету, для этого нажми /create')
-
+    await message.answer(f'Привет, {message.chat.first_name} {message.chat.last_name}! Для начала нужно заполнить анкету, для этого нажми /create', reply_markup=kb1(create))
+    await message.answer()
+    
 
 @dp.message_handler(commands=['create'])
 async def create_command(message: types.Message) -> None:
-    await message.answer('Давай же начнем! Для начала выбери свой отдел')
+    await message.answer('Давай же начнем! Для начала выбери свой отдел', reply_markup=kb1(dep))
     await QuestionnaireStatesGroup.department.set()
 
 @dp.message_handler(content_types=['text'], state=QuestionnaireStatesGroup.department)
@@ -52,15 +75,20 @@ async def load_department(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['department'] = message.text
 
-    await message.answer('Теперь укажи свой пол')
+    await message.answer('Теперь укажи свой пол',reply_markup=kb1(sx))
     await QuestionnaireStatesGroup.next()
+
+@dp.message_handler(lambda message: not message.text.isdigit(), state=QuestionnaireStatesGroup.age)
+async def check_age(message: types.Message):
+    await message.answer(f'{message.chat.first_name}, это не число!')
+
 
 @dp.message_handler(state=QuestionnaireStatesGroup.sex)
 async def load_sex(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['sex'] = message.text
 
-    await message.answer('Сколько тебе лет?')
+    await message.answer('Сколько тебе лет?',reply_markup=types.ReplyKeyboardRemove())
     await QuestionnaireStatesGroup.next()
 
 @dp.message_handler(state=QuestionnaireStatesGroup.age)
@@ -68,23 +96,18 @@ async def load_age(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['age'] = message.text
 
-    await message.answer('Теперь укажи своё хобби')
+    await message.answer('Теперь укажи своё хобби',reply_markup=kb1(hob))
     await QuestionnaireStatesGroup.next()
 
 @dp.message_handler(state=QuestionnaireStatesGroup.hobbies)
 async def load_hobbies(message: types.Message, state: FSMContext) -> None:
     async with state.proxy() as data:
         data['hobbies'] = message.text
-        await bot.send_message(chat_id=message.from_user.id, text=f"{data['department']},{data['sex']},{data['age']},{data['hobbies']}")
-
-    await message.answer('Ваша анкета успешно создана!)')
+        await bot.send_message(chat_id=message.from_user.id, text=f"Имя - {message.chat.first_name} {message.chat.last_name}\nОтдел - {data['department']}\nПол - {data['sex']}\nВозраст - {data['age']}\nХобби - {data['hobbies']}")
     await state.finish()
-
+        
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
- 
-
-
  
 
